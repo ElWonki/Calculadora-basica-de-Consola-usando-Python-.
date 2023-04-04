3dc0f3ab609a4d21ef0bb40a7c2226fa74807e14
ElWonki <95188293+ElWonki@users.noreply.github.com>
2023-04-04 13:47:27 -0500

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,28 @@
+import os
+
+# operaciones
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+input ()
+os.system("cls ")
+
+while True:
+    print("Calculadora")
+    print ("1 - Sumar 2 - Restar")
+    opcion = input ("ingrese el numero de la operacion a realizar")
+
+    if opcion=="1":
+        numero1 = float (input ("Numero 1: "))
+        numero2 = float (input ("Numero 2: "))
+                        
+        print("El resultado de la suma es: ",suma(numero1, numero2))
+
+    elif opcion=="2":
+        numero1 = float (input ("Numero 1: "))
+        numero2 = float (input ("Numero 2: "))
+                        
+        print("La resta es: ",resta(numero1, numero2))
